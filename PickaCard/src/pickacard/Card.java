45ed6296194a205485fe0bef4ad8d8b7ea94ba4e
hp <hp@DESKTOP-JQEF3UA>
2021-06-05 18:58:23 +0530

    initial commit

---
+++ PickaCard/src/pickacard/Card.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pickacard;
+
+/**
+ *A class that models playing card objects ,cards have
+ * a value ( note that ace = 1, jack = 11 , queen = 12 , king = 13 )
+ * a suit ( club , heart , spades , diamonds)
+ * there are 52 cards in a deck , no jokers,
+ * @author jaspreet kaur
+ * @date 5 June 2021
+ */
+public class Card {
+
+       private String suit ;//clubs, spades,diamonds,hearts
+    private int value ;//1-13
+    public static final String [] SUITS ={"Hearts","Diamond","Spades","Clibs"};
+   
+    /**
+     * @return the suit
+     */
+  public String getSuit(){
+      return suit ;
+  }
+  /**
+   * @param suit the suit to set
+   */
+  public void setSuit (String suit ){
+      this.suit = suit;
+      
+  }
+  /**
+   * @return the value 
+   */
+  public int getValue(){
+      return value ;
+      
+  }
+  /**
+   * @param value the value to set 
+   */
+  public void setValue ( int value) {
+      this.value = value;
+  }
+    }
---
+++ PickaCard/src/pickacard/CardTrick.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pickacard;
+
+/**
+ *a class that fills the magic hand of 7 cards with random card objects and then ask the user to pick a card and
+ * search the array of card for the match to the user's card .to be used aa starting code in ICE 1
+ * @author Jaspreet kaur
+ * @date 5 June 2021
+ */
+public class CardTrick {
+  public static void main(String[]args){
+        Card[] magicHand = new Card[7];
+        for(int i=0;i< magicHand.length;i++){
+            Card c = new Card();  
+              
+        }
+        
+    }
+  
+}
