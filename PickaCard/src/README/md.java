0e9f6af87dbcfbc709b7322d203266d9410cd640
hp <hp@DESKTOP-JQEF3UA>
2021-06-05 19:50:21 +0530

    add README FILE AS REQUESTED IN STEP 10

---
+++ PickaCard/src/README/md.java
@@ -0,0 +1,6 @@
+  # Jaspreetkaur7320
+  ## https://github.com/Jaspreetkaur7320/Jaspreetkaur7320
+  ## this project demonstrates the use of pick a card game 
+### Instructions on how to play a game ...
+  
+
